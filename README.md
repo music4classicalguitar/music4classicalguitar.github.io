90958146d90dc758cef454e94ebf107f5ae4d8f6
music4classicalguitar <music4classicalguitar@gmail.com>
2021-02-04 20:54:31 +0100

    Initial upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Documentation and demo
+
+[swf2js.js](https://music4classicalguitar.github.io/swf2js/index.html)
+
+swf2js.js is a JavaScript FlashPlayer emulator that analyzes SWF files of Adobe Animate (Flash) in real time and converts them to HTML.
---
+++ docs/swf2js/gen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cd /Library/WebServer/Documents/test/music4classicalguitar.github.io/docs/swf2js
+
+CreateFlashHtml()
+{
+  echo '<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>'"${NAME}"'</title>
+  </head>
+  <body>
+    <object width="100%" height="100%">
+      <param name="movie" value="'"${NAME}"'.swf">
+      <embed src="'"${NAME}"'.swf" width="100%" height="100%"></embed>
+    </object>
+  </body>
+</html>' > "${HTML_FILE}"
+}
+
+FILES=`ls *.swf`
+for FILE in ${FILES}
+  do
+  NAME=`echo "${FILE}" | sed 's/[.]swf//g'`
+  HTML_FILE="${NAME}_flash.html"
+  if [ ! -f "${HTML_FILE}" ]
+  then
+    CreateFlashHtml
+  fi
+  ls -l "${FILE}" "${NAME}.html" "${NAME}_flash.html"
+  echo
+  done
