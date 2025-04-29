a23a95957a5f79bc357bd9c164a45d0a57618353
music4classicalguitar <music4classicalguitar@gmail.com>
2025-04-29 14:57:18 +0200

    Update to swf2js 1.2.7

--- docs/swf2js/js/testbed.js
+++ docs/swf2js/js/testbed.js
@@ -10,31 +10,55 @@ var _audio = document.getElementById('audio');
 var _bgcolor = document.getElementById('bgcolor');
 var _overrule_bgcolor = document.getElementById('overrule_bgcolor');
 var _autoStart = document.getElementById('autoStart');
+var _scaling = document.getElementById('scaling');
 var _debug = document.getElementById('debug');
 
+var _play = document.getElementById('play');
+var _step = document.getElementById('step');
+var _stop = document.getElementById('stop');
+var _reload = document.getElementById('reload');
+var _show_info = document.getElementById('show_info');
+var _show_debug = document.getElementById('show_debug');
+
+_play.disabled = true;
+_step.disabled = true;
+_stop.disabled = true;
+_reload.disabled = true;
+_show_info.disabled = true;
+_show_debug.disabled = true;
+_play.addEventListener('click', play );
+_step.addEventListener('click', step );
+_stop.addEventListener('click', stop );
+_reload.addEventListener('click', reload );
+_show_info.addEventListener('click', info );
+_show_debug.addEventListener('click', debug );
+
 function resize() {
-		var rect = div_swf.getBoundingClientRect();
-		var vw = Math.max(div_swf.clientWidth || 0, window.innerWidth || 0)
-		var vh = Math.max(div_swf.clientHeight || 0, window.innerHeight || 0)
-		div_swf.style.width = Math.floor(vw - rect.left/2-20)+'px';
-		div_swf.style.height = Math.floor(vh - rect.top-20)+'px';
+	var rect = div_swf.getBoundingClientRect();
+	var vw = Math.max(div_swf.clientWidth || 0, window.innerWidth || 0)
+	var vh = Math.max(div_swf.clientHeight || 0, window.innerHeight || 0)
+	div_swf.style.width = Math.floor(vw - rect.left/2-20)+'px';
+	div_swf.style.height = Math.floor(vh - rect.top-20)+'px';
 }
 
 window.addEventListener('resize', function() {
 	resize();
-	if (_stage) _stage.resize();
+	//if (_stage) _stage.resize();
 });
+
+function clear() {
+	if (_stage) {
+		_stage.clear();
+		//_stage = null;
+	}
+};
 
 function input(e) {
 	if (e.target.files[0]) {
 		resize();
+		clear();
 		_swf_file = URL.createObjectURL(e.target.files[0]);
 		load(URL.createObjectURL(e.target.files[0]));
-		//document.getElementById('input').disabled = true; //removeEventListener('change', input);
-		document.getElementById('input').addEventListener('click', function(e) {
-			document.getElementById('input').disabled = true;
-			window.alert('Please refresh page to test another SWF-file');
-		});
 	}
 }
 
@@ -52,37 +76,44 @@ function load(obj) {
 		_options.bgcolor = _bgcolor.value;
 	if (_debug.options[_debug.selectedIndex].value==1)
 		_options.debug = true;
+	if (_scaling.options[_scaling.selectedIndex].value==0)
+		_options.noScaling = true;
 	var w = document.getElementById('width').value;
 	if (w && !isNaN(w)) _options.width = w;
 	var h = document.getElementById('height').value;
 	if (h && !isNaN(h)) _options.height = h;
 	_stage = swf2js.load(obj, _options);
-	document.getElementById('play').addEventListener('click', play );
-	document.getElementById('stop').addEventListener('click', stop );
-	document.getElementById('step').addEventListener('click', step );
-	document.getElementById('show_info').addEventListener('click', info );
-	document.getElementById('show_debug').addEventListener('click', debug );
+	_play.disabled = false;
+	_step.disabled = false;
+	_stop.disabled = false;
+	_reload.disabled = false;
+	_show_info.disabled = false;
+	//if (_debug.options[_debug.selectedIndex].value==1) {
+		_show_debug.disabled = false;
+	//} else {
+	//	_show_debug.disabled = true;		
+	//}
 }
 
 function play() {
-	console.log('Play');
 	if (_stage) _stage.play();
 }
 function step() {
-	console.log('Step');
 	if (_stage) _stage.step();
 }
 function stop() {
-	console.log('Stop');
 	if (_stage) _stage.stop();
 }
 
+function reload() {
+	if (_stage) _stage.clear();
+	if (_swf_file) load(_swf_file);
+}
+
 function info() {
-	console.log('Info');
 	if (_stage) _stage.showInfo();
 }
 
 function debug() {
-	console.log('Debug');
 	if (_stage) _stage.showDebug();
 }
